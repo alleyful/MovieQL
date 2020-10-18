debf07762190ba9da4dce521947655cbe86cb2cd
alleyful <alleyful@naver.com>
2020-10-18 14:38:26 +0900

    example mutation & query with movieDB

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: '0',
-    name: 'alley',
-    age: 20,
-    gender: "female"
+    id: 1,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: '1',
-    name: 'abc',
-    age: 18,
-    gender: "male"
+    id: 2,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: '2',
-    name: 'aaaaaa',
-    age: 32,
-    gender: "female"
+    id: 3,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: '3',
-    name: 'hi',
-    age: 23,
-    gender: "male"
-  },
-  {
-    id: '4',
-    name: 'smail',
-    age: 34,
-    gender: "male"
+    id: 4,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => String(id) === person.id);
-  return filteredPeople[0];
-}
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
